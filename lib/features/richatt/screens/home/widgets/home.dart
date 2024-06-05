11972bfbe1ee3f145b286db8da1809526d8ae3a1
adamarichatt <adama.sall@richatt.com>
2024-06-05 11:35:21 +0000

    Card favorie

--- lib/features/richatt/screens/home/widgets/home.dart
+++ lib/features/richatt/screens/home/widgets/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
@@ -5,6 +6,7 @@ import 'package:get/get.dart';
 import 'package:richatt_mobile_socle_v1/common/widgets/custom_shapes/containers/primary_header_container.dart';
 import 'package:richatt_mobile_socle_v1/common/widgets/custom_shapes/containers/search_container.dart';
 import 'package:richatt_mobile_socle_v1/common/widgets/doctor/RProfileCard.dart';
+import 'package:richatt_mobile_socle_v1/common/widgets/doctor/RProfileCardVertical.dart';
 import 'package:richatt_mobile_socle_v1/common/widgets/doctor/RdoctorCardVertical.dart';
 import 'package:richatt_mobile_socle_v1/features/richatt/controllers/professionalController.dart';
 import 'package:richatt_mobile_socle_v1/features/richatt/screens/home/widgets/home_appb.dart';
@@ -45,21 +47,28 @@ class HomeScreen extends StatelessWidget {
                 ],
               ),
             ),
-            Container(
-              width: RDeviceUtils.getScreenWidth(context),
-              height: 300, // Ensure that the container has an explicit height
-              child: Obx(
-                () => SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: List.generate(
-                      controller.featuredProf.length,
-                      (index) => RDoctorCardVertical(
-                        professional: controller.featuredProf[index],
+            Padding(
+              padding: const EdgeInsets.all(RSizes.defaultSpace),
+              child: Column(
+                children: [
+                  Obx(
+                    () => SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: List.generate(
+                          controller.featuredProf.length,
+                          (index) => Padding(
+                            padding: const EdgeInsets.only(
+                                right: 16.0), // Espace entre les cartes
+                            child: ProfileCardVertical(
+                              professional: controller.featuredProf[index],
+                            ),
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
+                ],
               ),
             ),
             Padding(
