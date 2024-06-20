3d87953ea6a52ca3fb9edbb59b512d91bbff5c86
Med-Mahmoud-Djeba <mohamed-mahmoud.djeba@richatt.com>
2024-06-20 16:49:32 +0100

    commit02_20/06

--- lib/features/richatt/screens/home/widgets/edit_appointment_page.dart
+++ lib/features/richatt/screens/home/widgets/edit_appointment_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import 'package:intl/intl.dart';
 import 'package:richatt_mobile_socle_v1/features/richatt/models/Appointment.dart';
 import 'package:richatt_mobile_socle_v1/features/richatt/models/Schedule.dart';
@@ -9,12 +8,18 @@ import 'package:richatt_mobile_socle_v1/features/richatt/screens/home/widgets/Ap
 import 'package:richatt_mobile_socle_v1/utils/constants/sizes.dart';
 import 'package:richatt_mobile_socle_v1/features/richatt/controllers/professionalController.dart';
 import 'package:collection/collection.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class EditAppointmentPage extends StatefulWidget {
   final Appointment appointment;
   final String email;
   final String phone;
-  EditAppointmentPage({required this.appointment, required this.email, required this.phone});
+
+  EditAppointmentPage({
+    required this.appointment,
+    required this.email,
+    required this.phone,
+  });
 
   @override
   _EditAppointmentPageState createState() => _EditAppointmentPageState();
@@ -30,6 +35,10 @@ class _EditAppointmentPageState extends State<EditAppointmentPage> {
   List<Service> services = [];
   String? selectedReason;
   final ProfessionalController _controller = ProfessionalController();
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  bool _showCalendar = false;
+  Schedule? newSchedule;
 
   @override
   void initState() {
@@ -91,11 +100,15 @@ class _EditAppointmentPageState extends State<EditAppointmentPage> {
       bool dateTimeChanged = newDateTime != initialDate;
 
       widget.appointment.dateTime = newDateTime.toIso8601String();
-      widget.appointment.firstName = firstNameController.text;
-      widget.appointment.lastName = lastNameController.text;
+      widget.appointment.firstName = firstNameController.value.text;
+      widget.appointment.lastName = lastNameController.value.text;
       widget.appointment.reason = service.name;
       widget.appointment.price = service.price;
       widget.appointment.duration = service.duration;
+      widget.appointment.email = widget.email;
+      widget.appointment.phone = widget.phone;
+      widget.appointment.address = widget.appointment.address;
+      widget.appointment.description = widget.appointment.description;
 
       if (dateTimeChanged) {
         List<Schedule> allSchedules = await _controller
@@ -112,15 +125,6 @@ class _EditAppointmentPageState extends State<EditAppointmentPage> {
           throw Exception('Original schedule not found');
         }
 
-        Schedule? newSchedule = allSchedules.firstWhereOrNull(
-          (schedule) =>
-              DateTime.parse(schedule.dateTime.split('T').first +
-                      ' ' +
-                      schedule.dateTime.split('T').last.split('-').first)
-                  .isAtSameMomentAs(newDateTime) &&
-              schedule.status == 'Active',
-        );
-
         if (newSchedule == null) {
           ScaffoldMessenger.of(context).showSnackBar(
             SnackBar(content: Text('No available slot at the chosen time')),
@@ -129,16 +133,16 @@ class _EditAppointmentPageState extends State<EditAppointmentPage> {
           await _controller.updateAppointment(
               widget.appointment.id!, widget.appointment);
           await _controller.enableSchedules([originalSchedule]);
-          await _controller.deleteSchedules([newSchedule]);
-          await _controller.addSchedules([newSchedule]);
-          await _controller.reserveSchedules([newSchedule]);
+          await _controller.deleteSchedules([newSchedule!]);
+          await _controller.addSchedules([newSchedule!]);
+          await _controller.reserveSchedules([newSchedule!]);
         }
       } else {
         await _controller.updateAppointment(
             widget.appointment.id!, widget.appointment);
       }
 
-      Get.to(() => AppointmentsList(email:widget.email, phone:widget.phone));
+      Get.to(() => AppointmentsList(email: widget.email, phone: widget.phone));
     } catch (error) {
       Future.delayed(Duration(milliseconds: 300), () {
         ScaffoldMessenger.of(context).showSnackBar(
@@ -160,39 +164,64 @@ class _EditAppointmentPageState extends State<EditAppointmentPage> {
           children: [
             const SizedBox(height: RSizes.spaceBtwInputFields),
             ListTile(
+              //       final formattedDate = DateFormat('dd/MM/yyyy').format(dateTime);
+              // final formattedTime = DateFormat('HH:mm').format(dateTime);
               title: Text(
-                  "Date: ${DateFormat('yyyy-MM-dd').format(selectedDate)}"),
+                  "Date: ${DateFormat('dd/MM/yyyy').format(selectedDate)}"),
+              subtitle: Text("Heure: ${DateFormat('HH:mm').format(DateTime(0, 1, 1, selectedTime.hour, selectedTime.minute))}"),
+
               trailing: Icon(Icons.calendar_today),
-              onTap: () async {
-                DateTime? picked = await showDatePicker(
-                  context: context,
-                  initialDate: selectedDate,
-                  firstDate: DateTime(2000),
-                  lastDate: DateTime(2101),
-                );
-                if (picked != null && picked != selectedDate) {
-                  setState(() {
-                    selectedDate = picked;
-                  });
-                }
+              onTap: () {
+                setState(() {
+                  _showCalendar = !_showCalendar;
+                });
               },
             ),
-            const SizedBox(height: RSizes.spaceBtwInputFields),
-            ListTile(
-              title: Text("Heure: ${selectedTime.format(context)}"),
-              trailing: Icon(Icons.access_time),
-              onTap: () async {
-                TimeOfDay? picked = await showTimePicker(
-                  context: context,
-                  initialTime: selectedTime,
-                );
-                if (picked != null && picked != selectedTime) {
+            if (_showCalendar) ...[
+              const SizedBox(height: RSizes.spaceBtwInputFields),
+              TableCalendar(
+                firstDay: DateTime(2020, 1, 1),
+                lastDay: DateTime(2030, 12, 31),
+                focusedDay: _focusedDay,
+                calendarFormat: _calendarFormat,
+                selectedDayPredicate: (day) {
+                  return isSameDay(selectedDate, day);
+                },
+                onDaySelected: (selectedDay, focusedDay) {
                   setState(() {
-                    selectedTime = picked;
+                    selectedDate = selectedDay;
+                    _focusedDay = focusedDay;
                   });
-                }
-              },
-            ),
+                },
+                onFormatChanged: (format) {
+                  if (_calendarFormat != format) {
+                    setState(() {
+                      _calendarFormat = format;
+                    });
+                  }
+                },
+              ),
+              const SizedBox(height: RSizes.spaceBtwInputFields),
+              FutureBuilder<List<Schedule>>(
+                future: _controller.fetchDaySchedules(
+                    selectedDate, widget.appointment.professional!.id!),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return Center(
+                        child: Text('No active schedules for selected day'));
+                  } else {
+                    List<Schedule> schedules = snapshot.data!;
+                    schedules.sort((a, b) => a.dateTime.compareTo(b.dateTime));
+
+                    return _activeSchedulesForSelectedDay(schedules);
+                  }
+                },
+              ),
+            ],
             const SizedBox(height: RSizes.spaceBtwInputFields),
             TextField(
               controller: firstNameController,
@@ -228,6 +257,42 @@ class _EditAppointmentPageState extends State<EditAppointmentPage> {
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _activeSchedulesForSelectedDay(List<Schedule> schedules) {
+    if (schedules.isEmpty) {
+      return Center(child: Text('No active schedules for the selected day'));
+    }
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Wrap(
+        spacing: 8.0, // Espace horizontal entre les boutons
+        runSpacing: 8.0, // Espace vertical entre les lignes de boutons
+        children: schedules.map((schedule) {
+          return ElevatedButton(
+            onPressed: () {
+              setState(() {
+                selectedDate = DateTime.parse(
+                    schedule.dateTime.split('T').first +
+                        ' ' +
+                        schedule.dateTime.split('T').last.split('-').first);
+                selectedTime = TimeOfDay(
+                  hour: int.parse(
+                      schedule.dateTime.split('T').last.split(':').first),
+                  minute: int.parse(
+                      schedule.dateTime.split('T').last.split(':')[1]),
+                );
+                newSchedule = schedule;
+                _showCalendar = false;
+              });
+            },
+            child: Text(schedule.dateTime
+                .substring(11, 16)), // Afficher seulement l'heure (HH:MM)
+          );
+        }).toList(),
       ),
     );
   }
