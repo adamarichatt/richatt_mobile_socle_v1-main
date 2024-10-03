ca29dd6a1265720d4fd52e59de94943cacdffb09
Med-Mahmoud-Djeba <mohamed-mahmoud.djeba@richatt.com>
2024-10-03 11:09:34 +0100

    fvv

--- lib/utils/constants/api_constants.dart
+++ lib/utils/constants/api_constants.dart
@@ -1,5 +1,6 @@
 class APIConstants {
   static const String rSecretAPIKey = "";
-  static const String apiBackend = "http://195.35.25.110:8733/api/";
-  static const String apiBackend1 = "http://localhost:8080/api/";
+  static const String apiBackend1 = "http://195.35.25.110:8733/api/";
+  static const String apiBackend= "http://localhost:8080/api/";
 }
+  
